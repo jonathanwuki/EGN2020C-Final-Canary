cd9b7ca275d45bb27f06bc3a9ceb3e9e2703cce2
Jonathan Wukitsch <11484046+jonathanwuki@users.noreply.github.com>
2022-11-16 13:39:57 -0500

    Update EGN2020C_Final.ino

--- EGN2020C_Final.ino
+++ EGN2020C_Final.ino
@@ -1,33 +1,44 @@
-#define PIEZO_PIN 9
-#define LED_PIN 13
-#define TEMP_HUM_PIN A0
-
-// defines for the frequency of the notes (.5 x freq of mid C)
-#define TONE_SOUND_1    330     // 658 Hz
-#define TONE_SOUND_2    370     // 740 Hz
-#define TONE_SOUND_3    392     // 784 Hz
-
-// defines for the duration of the notes (in ms)
-#define TONE_DURATION     170
-#define TONE_WAIT_TIME    512
+/*
+ * "Canary" final project source code
+ * Author: Jonathan Wukitsch
+ *         EGN2020C - Engineering Design & Society
+ *         Fall 2022
+ *         University of Florida
+ */
 
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include <dht.h>
 #include "Adafruit_SGP30.h"
 
+#define PIEZO_PIN 9 // Piezo speaker pin - Digital 9
+#define LED_PIN 13 // LED pin - Digital 13
+#define TEMP_HUM_PIN A0 // Temperature/humidity sensor pin - Analog A0
+
+// Defines for the frequency of the piezo notes (.5 x freq of mid C)
+#define TONE_SOUND_1 330 // 658 Hz
+#define TONE_SOUND_2 370 // 740 Hz
+#define TONE_SOUND_3 392 // 784 Hz
+
+// Defines for the duration of the piezo notes (in ms)
+#define TONE_DURATION     170
+#define TONE_WAIT_TIME    512
+
+const bool IS_DEBUG_ENABLED = true;
 bool isAboveThreshold = false;
 bool calibrated = false;
 
+// Initialize SGP30 sensor
 Adafruit_SGP30 sgp;
+
+// Initialize temp/humidity sensor
 dht DHT;
 
-// Initialize the library by associating any needed LCD interface pin
-// with the Arduino pin number it is connected to
+// Initialize LCD library
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-/* return absolute humidity [mg/m^3] with approximation formula
+/* Return absolute humidity [mg/m^3] with approximation formula
 * @param temperature [°C]
 * @param humidity [%RH]
 */
@@ -38,65 +49,61 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity) {
     return absoluteHumidityScaled;
 }
 
-void play_tune(int tune) {               // play a tune . . .
-  switch (tune) {                       // a case for each tune 
-  case 1:                             // 007  E, F#, G (mult by 2 for next higher octave)
-    ToneOut(TONE_SOUND_1*2, TONE_DURATION);                    
-    ToneOut(TONE_SOUND_2*2, TONE_DURATION);
-    ToneOut(TONE_SOUND_3*2, TONE_DURATION);
-    delay(TONE_WAIT_TIME);
-    break;
-  }
+void playStartupTone() {
+  ToneOut(TONE_SOUND_1 * 2, TONE_DURATION);                    
+  ToneOut(TONE_SOUND_2 * 2, TONE_DURATION);
+  ToneOut(TONE_SOUND_3 * 2, TONE_DURATION);
+  delay(TONE_WAIT_TIME);
 }
 
 void ToneOut(int pitch, int duration) {  // pitch in Hz, duration in ms
   int delayPeriod;
   long cycles, i;
 
-  pinMode(PIEZO_PIN, OUTPUT);           // turn on output pin
+  pinMode(PIEZO_PIN, OUTPUT); // Turn on output pin
   delayPeriod = (500000 / pitch) - 7;   // calc 1/2 period in us -7 for overhead
   cycles = ((long)pitch * (long)duration) / 1000; // calc. number of cycles for loop
 
-  for (i = 1; i <= cycles; i++) {           // play note for duration ms 
+  for (i = 1; i <= cycles; i++) { // Play noise for specific duration
     digitalWrite(PIEZO_PIN, HIGH); 
     delayMicroseconds(delayPeriod);
     digitalWrite(PIEZO_PIN, LOW); 
-    delayMicroseconds(delayPeriod - 1); // - 1 to make up for digitaWrite overhead
+    delayMicroseconds(delayPeriod - 1); // - 1 to make up for digitalWrite overhead
   }
   
-  pinMode(PIEZO_PIN, INPUT);            // shut off pin to avoid noise from other operations
+  pinMode(PIEZO_PIN, INPUT); // Shut off pin to avoid noise from other operations
 }
 
 void setup() {
   Serial.begin(115200);
   while(!Serial) { delay(10); } // Wait for serial console to open
 
-  Serial.println("SGP30 Initialized!");
-
   if(!sgp.begin()) {
     Serial.println("Sensor not found :(");
-    while (1);
+    while(1);
+  }
+
+  if(IS_DEBUG_ENABLED) {
+    Serial.println("SGP30 initialized!");
+    Serial.print("Found SGP30 serial #");
+    Serial.print(sgp.serialnumber[0], HEX);
+    Serial.print(sgp.serialnumber[1], HEX);
+    Serial.println(sgp.serialnumber[2], HEX);
   }
   
-  Serial.print("Found SGP30 serial #");
-  Serial.print(sgp.serialnumber[0], HEX);
-  Serial.print(sgp.serialnumber[1], HEX);
-  Serial.println(sgp.serialnumber[2], HEX);
-  
-  // set up the LCD's number of columns and rows
+  // LCD setup
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("Initializing...");
-  lcd.setCursor(0, 1); // Setting the cursor on LCD
-  lcd.print("FW Version: 1.54"); // prints on LCD
+  lcd.setCursor(0, 1);
+  lcd.print("FW Version: 1.54");
   delay(1000);
 
-  play_tune(1);
+  playStartupTone();
   pinMode(PIEZO_PIN, OUTPUT); // Piezo
   pinMode(LED_PIN, OUTPUT); // LED
 
   // If you have a baseline measurement from before you can assign it to start, to 'self-calibrate'
-  //sgp.setIAQBaseline(0x8E68, 0x8F41);  // Will vary for each sensor!
+  // sgp.setIAQBaseline(0x8E68, 0x8F41);  // Will vary for each sensor!
 }
 
 int counter = 0;
